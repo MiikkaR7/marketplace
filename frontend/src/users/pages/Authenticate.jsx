3177eb4d15521cc6d392105acbbf513bbabc2ba9
Miikka Riipi <miikka.riipi@tuni.fi>
2024-04-09 14:39:13 +0300

    auth page api methods

--- frontend/src/users/pages/Authenticate.jsx
+++ frontend/src/users/pages/Authenticate.jsx
@@ -1,4 +1,11 @@
-import React from "react";
+import { useRef, useState, useContext } from "react";
+import { useMutation } from "react-query";
+
+import { signUpUser, logInUser } from "../api/users";
+
+import Input from "../../shared/components/Input";
+import Button from "../../shared/components/Button";
+
 import './Authenticate.css';
 
 const Authenticate = () => {
