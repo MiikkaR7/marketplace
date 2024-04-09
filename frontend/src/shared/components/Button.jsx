10a08eeb97d76a6a932c10262d8afff685ccc04a
Miikka Riipi <miikka.riipi@tuni.fi>
2024-04-09 13:27:27 +0300

    more styling and button component

---
+++ frontend/src/shared/components/Button.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import './Button.css';
+
+const Button = (props) => {
+
+    return (
+        <button
+        className={`button button--${props.size || 'default'} 
+        ${props.inverse && 'button--inverse'}
+        ${props.danger && 'button--danger'}`}
+        id={props.id}
+        type={props.type}
+        onClick={props.onClick}
+        disabled={props.disabled}
+        >
+        {props.children}
+        </button>
+);
+
+};
+
+export default Button;
