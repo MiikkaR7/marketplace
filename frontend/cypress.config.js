403e0263c85d0cfd1c4c4b8ef04826a69921ae2c
Miikka Riipi <miikka.riipi@tuni.fi>
2024-04-22 12:16:32 +0300

    frontend tests 3

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,7 +3,7 @@ export default defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
-    baseUrl: 'http://172.16.4.202:5030',
+    baseUrl: 'http://172.16.4.202:90',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
