4508cd3719a8a2e2395c319e98ca55a0645c9acc
Miikka Riipi <miikka.riipi@tuni.fi>
2024-04-22 12:25:14 +0300

    frontend tests can be run locally

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,7 +3,7 @@ export default defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
-    baseUrl: 'http://172.16.4.202:90',
+    baseUrl: 'http://localhost:5172',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
